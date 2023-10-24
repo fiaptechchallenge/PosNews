dca0e2dbb310f8698ada87cddb3ee671909e4719
Kaique Leonardo <kaiqueleonardo18@gmail.com>
2023-10-24 12:56:55 -0300

    iniciando testes de integracao de noticias

---
+++ PosNews_Testes/Builders/RegisterNoticiaBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using PosNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosNews_Testes.Builders
+{
+    public class RegisterNoticiaBuilder : Faker<RegisterNoticia>
+    {
+        public RegisterNoticiaBuilder()
+        {
+            RuleFor(n => n.Titulo, f => f.Lorem.Text());
+            RuleFor(n => n.Descricao, f => f.Lorem.Paragraph());
+            RuleFor(n => n.Chapeu, f => f.Name.FullName());
+            RuleFor(n => n.DataPublicacao, DateTime.Now);
+            RuleFor(n => n.Autor, f => f.Name.FullName());
+        }
+    }
+}
