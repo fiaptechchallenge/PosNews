990d12ab2fade6955fc55f1c0b3bdaa6830f418f
nathalia <nathalia.souza@codeblox.com.br>
2023-10-25 17:34:53 -0300

    Adiciona teste p/ GetAllAsync

---
+++ PosNews_Testes/Unit/Application/Noticias/GetAllAsyncTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Infraestrutura.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PosNews.Controllers;
+using PosNews.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosNews_Testes.Unit.Application.Noticias
+{
+    public class GetAllAsyncTests
+    {
+        [Fact]
+        public async Task ListaNoticia()
+        {
+            var noticiaRepository = Mock.Of<INoticiaRepository>();
+            var mapper = Mock.Of<IMapper>();
+
+            var noticias = new List<Noticia>()
+            {
+                new Noticia("Titulo Teste 1", "Descrição Teste", "Chapeu Teste", "Autor Teste"),
+                new Noticia("Titulo Teste 2", "Descrição Teste", "Chapeu Teste", "Autor Teste"),
+                new Noticia("Titulo Teste 3", "Descrição Teste", "Chapeu Teste", "Autor Teste"),
+            };
+
+            Mock.Get(noticiaRepository)
+                .Setup(x => x.GetAllAsync(It.IsAny<Expression<Func<Noticia, bool>>>()))
+                .ReturnsAsync(noticias);
+
+            var noticiaController = new NoticiaController(noticiaRepository, mapper);
+
+            var result = await noticiaController.GetAllAsync();
+
+            var objectResult = Assert.IsType<OkObjectResult>(result.Result);
+
+            objectResult.StatusCode.Should().Be(200);
+            objectResult.Value.Should().NotBeNull();
+        }
+    }
+}
